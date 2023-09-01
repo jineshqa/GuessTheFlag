656743bb0583ce136975efcfded62a5a6a662bad
Jinesh Doshi <jdoshi@indeed.com>
2023-09-01 10:39:50 -0700

    Changed directories

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jinesh Doshi on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
